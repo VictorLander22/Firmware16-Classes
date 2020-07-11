cd404ed4c5509ebca7b7599c4d76f612f8c36fa1
Fernando <fcleal@fcleal.com.br>
2020-07-11 15:20:49 -0300

    Protocolo RF

--- firmware16.ino
+++ firmware16.ino
@@ -189,7 +189,7 @@ int rxRF = 12;
 int txRF = 15;
 //boolean enReadRF = false;
 int tamanhoRF = -1;
-gProtocoloRF
+int gProtocoloRF = 1;
 String codigoRF = "-1";
 boolean SensorRFAlterado = true;
 String SensoresRF[30] = {"", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
