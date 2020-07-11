8f85e43365e8b6b778d6181428f11a7703df345d
Fernando <fcleal@fcleal.com.br>
2020-07-11 15:20:10 -0300

    Protocolo RF

--- firmware16.ino
+++ firmware16.ino
@@ -189,6 +189,7 @@ int rxRF = 12;
 int txRF = 15;
 //boolean enReadRF = false;
 int tamanhoRF = -1;
+gProtocoloRF
 String codigoRF = "-1";
 boolean SensorRFAlterado = true;
 String SensoresRF[30] = {"", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""};
@@ -624,6 +625,7 @@ void loop(void)
         Serial.print("bit ");
         Serial.print("Protocol: ");
         Serial.println(mySwitch.getReceivedProtocol());
+        gProtocoloRF = mySwitch.getReceivedProtocol();
         trataRF();
       }
       mySwitch.resetAvailable();
