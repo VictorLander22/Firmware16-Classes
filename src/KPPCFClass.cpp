68b06831ddf7bf7e20b14d99b0cffc2b6ff3b55c
VictorLander <victordlander@hotmail.com>
2022-02-07 08:37:47 -0300

    first commit

--- src/KPPCFClass.cpp
+++
@@ -1,7 +0,0 @@
-#include "KPPCFClass.h"
-
-void KPPCF::begin() {}
-uint8_t KPPCF::read8() {}
-uint8_t KPPCF::read(const uint8_t pin) {}
-void KPPCF::write8(const uint8_t value) {}
-void KPPCF::write(const uint8_t pin, const uint8_t value) {}
